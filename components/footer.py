c2516fe646a5b1c6bed77112a7d2aa306632f057
drqrack <chrisappia461@gmail.com>
2025-09-04 16:48:27 +0000

    added social-buttons

--- components/footer.py
+++ components/footer.py
@@ -1,8 +1,10 @@
-from nicegui import ui
+from nicegui import ui 
 
-ui.add_head_html('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">') 
+
 
 def show_footer():
+    ui.add_head_html('<script src="https://kit.fontawesome.com/ccba89e5d4.js" crossorigin="anonymous"></script>')
+
     with ui.element("div").classes("w-full bg-blue-800 items-center justify-center px-10 py-5"):
         with ui.row().classes("items-center justify-center text-4xl font-bold mb-4"):
             ui.label("Event").classes("text-white")
@@ -14,17 +16,17 @@ def show_footer():
 
         ui.html("<hr>").classes("mt-16")
 
-        with ui.element("div").classes("flex flex-row justify-between text-white px-4 py-8"):
+        with ui.element("div").classes("flex flex-row justify-between text-white px-4 py-8 items-center text-center"):
             with ui.row():
                 ui.label("English")
                 ui.label("French")
                 ui.label("Twi")
 
-            with ui.row().classes("text-white text-4xl"):
-                ui.html('<i class="fa-brands fa-facebook"></i>')
+            with ui.row().classes("text-white text-xl"):
+                ui.html('<i class="fa-brands fa-facebook"></i>').classes("hover:text")
                 ui.html('<i class="fa-brands fa-instagram"></i>')
                 ui.html('<i class="fa-brands fa-twitter"></i>')
 
             with ui.row():
-                ui.label("Non Copyrighted &copy 2025 Upload by dr. crack studio")
+                ui.html("<p>Non Copyrighted &copy; 2025 Upload by dr. crack studio</p>")
 
